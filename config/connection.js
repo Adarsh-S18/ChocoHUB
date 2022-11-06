75cd0206a5425206f96be278bcc509c81a6eb7c3
Adarsh S Pillai <adarshspillai2001@gmail.com>
2022-11-06 15:32:19 +0530

    Changed to ATLAS

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
 const mongoClient=require('mongodb').MongoClient
 const state={db:null}
+require('dotenv').config
 
 module.exports.connect=function(done)
 {
-    const url='mongodb://localhost:27017'
+    const url=`mongodb+srv://adarsh18:${process.env.PASSWORD}@cluster0.w3h1ja9.mongodb.net/?retryWrites=true&w=majority`
     const dbname='hub'
     mongoClient.connect(url,(err,data)=>{
         if(err)
